4acaa997f78b0a81a7535c2a410a7c94343360ef
fahimali <fammyali1@gmail.com>
2024-10-25 11:59:07 +0500

    ACE-392

--- Assets/Scripts/Api/NoodleApi.cs
+++ Assets/Scripts/Api/NoodleApi.cs
@@ -6,7 +6,7 @@ public class NoodleApi
 {
     public static void GetBalance(UnityAction<string> _success = null, UnityAction _error = null)
     {
-        SwaggerAPIManager.Instance.SendGetAPI($"/api/app/games/ace/balance/{DataManager.AccessCode}/{DataManager.MemberId}", _success, _error);
+        SwaggerAPIManager.Instance.SendGetAPI($"/api/app/games/ace/balance/{DataManager.MemberId}/{DataManager.AccessCode}", _success, _error);
     }
 
     public static void GetTableAvailableChips(UnityAction<string> _success = null, UnityAction _error = null)
