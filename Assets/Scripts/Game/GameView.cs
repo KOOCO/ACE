a8b3edad71ce7ad286769a3a480a60017d35461e
fahimali <fammyali1@gmail.com>
2024-10-31 12:00:25 +0500

    Finish round test

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -2560,6 +2560,7 @@ public class GameView : MonoBehaviour
                 }
             }
         }
+        Debug.Log("Game Room Details  ::" + gameRoomData);
         // Add all players' hand data to the result
         foreach (var player in gameRoomData.playerDataDic.Values) // Assuming this contains all players in the game
         {
