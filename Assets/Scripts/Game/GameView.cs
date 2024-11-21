3a6e64e106ff9290f3cffa8f4f6f6283be33c05f
fahimali <fammyali1@gmail.com>
2024-11-21 13:33:33 +0500

    ACE-359

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -1061,7 +1061,7 @@ public class GameView : MonoBehaviour
         set
         {
             Debug.Log($"SetActionButton called with value: {value}");
-
+            SetActingButtonEnable = value;
             thisData.isLocalPlayerTurn = value;
 
             UpdateActionBtns();
