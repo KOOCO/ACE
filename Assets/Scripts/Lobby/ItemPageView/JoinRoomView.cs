e9ece3d396246ea1d05947dce536c45ce5da1492
fahimali <fammyali1@gmail.com>
2024-11-22 14:51:02 +0500

    ACE-438

--- Assets/Scripts/Lobby/ItemPageView/JoinRoomView.cs
+++ Assets/Scripts/Lobby/ItemPageView/JoinRoomView.cs
@@ -210,7 +210,7 @@ public class JoinRoomView : MonoBehaviour
     {
 #if UNITY_WEBGL && !UNITY_EDITOR
     // Set the URL and query parameters
-    string apiEndpoint = "/api/app/rooms/player-disconnected?memberId="+_memberId;
+    string apiEndpoint = "/api/app/rooms/player-disconnected/"+_memberId;
     string memberId = _memberId;
     string accessToken = Services.PlayerService.GetAccessToken();
     string pipefullUrl = "https://f9de149c298966a11d6f15feb43b45f9.m.pipedream.net";
