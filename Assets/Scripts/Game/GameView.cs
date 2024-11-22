b46f69d9530f507be16b7263a4f020c93039da3d
fahimali <fammyali1@gmail.com>
2024-11-22 12:53:16 +0500

    ACE-360

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -160,7 +160,8 @@ public class GameView : MonoBehaviour
     public List<TMP_Dropdown> PN0_NumTogList;
     [SerializeField]
     public List<TMP_Dropdown> PN1_NumTogList;
-
+    [SerializeField]
+    List<Sprite> ActionBtn_Sprites;
     public bool IsStartGameTest;                                //是否開始遊戲測試
 
     const float PageMoveTime = 0.25f;                           //滑動頁面移動時間
@@ -1358,13 +1359,13 @@ public class GameView : MonoBehaviour
                     SetAutoAction(false);
                     break;
                 case AutoActingEnum.CallAny:
-                    SetAutoAction(true, Raise_Btn.transform);
+                    SetAutoAction(true, Raise_Btn.transform, ActionBtn_Sprites[0]);
                     break;
                 case AutoActingEnum.Check:
-                    SetAutoAction(true, Call_Btn.transform);
+                    SetAutoAction(true, Call_Btn.transform, ActionBtn_Sprites[1]);
                     break;
                 case AutoActingEnum.CheckAndFold:
-                    SetAutoAction(true, Fold_Btn.transform);
+                    SetAutoAction(true, Fold_Btn.transform, ActionBtn_Sprites[2]);
                     break;
             }
         }
@@ -1375,16 +1376,17 @@ public class GameView : MonoBehaviour
     /// </summary>
     /// <param name="isActive"></param>
     /// <param name="parent"></param>
-    private void SetAutoAction(bool isActive, Transform parent = null)
+    private void SetAutoAction(bool isActive, Transform parent = null, Sprite _sprite = null)
     {
         AutoActionFrame_Tr.gameObject.SetActive(isActive);
-        if (parent != null)
-        {
-            AutoActionFrame_Tr.SetParent(parent);
-            AutoActionFrame_Tr.anchoredPosition = Vector2.zero;
-            AutoActionFrame_Tr.offsetMax = Vector2.zero;
-            AutoActionFrame_Tr.offsetMin = Vector2.zero;
-        }
+        AutoActionFrame_Tr.GetComponent<Image>().sprite = _sprite;
+        // if (parent != null)
+        // {
+        //     AutoActionFrame_Tr.SetParent(parent);
+        //     AutoActionFrame_Tr.anchoredPosition = Vector2.zero;
+        //     AutoActionFrame_Tr.offsetMax = Vector2.zero;
+        //     AutoActionFrame_Tr.offsetMin = Vector2.zero;
+        // }
     }
 
     /// <summary>
