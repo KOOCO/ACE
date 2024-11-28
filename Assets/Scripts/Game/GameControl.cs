8d19ce9b0198e4d39ceb31967ae992ebec27bf86
Shoaib Watto <shoaibwattoo431@gmail.com>
2024-11-28 08:55:24 +0500

    ACE-565

--- Assets/Scripts/Game/GameControl.cs
+++ Assets/Scripts/Game/GameControl.cs
@@ -2656,7 +2656,7 @@ public class GameControl : MonoBehaviour
                 Debug.Log($"GameControl :: {player.nickname} : Input Hand :: {fullHand}");
                 // Store hand rank and match poker for tie-breaking
                 Debug.Log($"GameControl :: {player.nickname} : Returned Cards :: {string.Join(" , ", matchPoker)}");
-                bool isStraight = result == 6 ? true : false;
+                bool isStraight = result == 6 || result == 2 ? true : false;
                 var _matchPoker = CalculateRank(matchPoker, isStraight);
                 Debug.Log($"GameControl :: {player.nickname} : Hand :: {PokerShape.HandRanks[result]} : CardsRank :: {string.Join(" , ", _matchPoker)}");
 
