9d4919f6ff5187415fe67af52e90e21918b687dc
Shoaib Watto <shoaibwattoo431@gmail.com>
2024-12-05 13:19:30 +0500

    ACE-603

--- Assets/Scripts/Game/GameView.cs
+++ Assets/Scripts/Game/GameView.cs
@@ -2999,6 +2999,7 @@ public class GameView : MonoBehaviour
 
     IEnumerator SaveResult(GameRoomData gameRoomData, bool isAllPlayerLeft = false)
     {
+        SetWinnerStringTxt = "";
         if (gameRoomData == null || gameRoomData.playingPlayersIdList == null)
         {
             Debug.LogError("SaveResult: Invalid gameRoomData or missing player list.");
