ebc8f1073ac4e0a8526e1c28553bcecfa86d7ca7
unknown <king24371@gmail.com>
2025-04-02 13:43:11 +0800

    改回舊版

--- Assets/Scripts/Game/RobotControl.cs
+++ Assets/Scripts/Game/RobotControl.cs
@@ -34,65 +34,65 @@ public class RobotControl : MonoBehaviour
         BetActingEnum action = BetActingEnum.None;
         double betValue = 0;
         #region old Editor
-        //int foldRate = isTest ? 0 : new System.Random().Next(0, 100);
+        int foldRate = isTest ? 0 : new System.Random().Next(0, 100);
 
-        ////是否只能All In
-        //bool isJustAllIn = robotData.carryChips <= gameRoomData.currCallValue;
-        ////首位加注玩家
-        //bool isFirst = gameRoomData.actionPlayerCount == 0;
+        //是否只能All In
+        bool isJustAllIn = robotData.carryChips <= gameRoomData.currCallValue;
+        //首位加注玩家
+        bool isFirst = gameRoomData.actionPlayerCount == 0;
 
-        //if (foldRate > 0)
-        //{
-        //    action = BetActingEnum.Call;
+        if (foldRate > 0)
+        {
+            action = BetActingEnum.Call;
 
-        //    if (isJustAllIn)
-        //    {
-        //        action = BetActingEnum.AllIn;
-        //        betValue = robotData.carryChips;
-        //    }
-        //    else
-        //    {
-        //        if (isFirst == true)
-        //        {
-        //            if (robotData.currAllBetChips == gameRoomData.currCallValue)
-        //            {
-        //                action = BetActingEnum.Check;
-        //            }
-        //            else
-        //            {
-        //                betValue = gameRoomData.currCallValue;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            if (robotData.currAllBetChips == gameRoomData.currCallValue)
-        //            {
-        //                action = BetActingEnum.Check;
-        //            }
-        //            else
-        //            {
-        //                betValue = gameRoomData.currCallValue;
-        //            }
-        //        }
-        //    }
-        //}
-        //else
-        //    action = BetActingEnum.Fold;
+            if (isJustAllIn)
+            {
+                action = BetActingEnum.AllIn;
+                betValue = robotData.carryChips;
+            }
+            else
+            {
+                if (isFirst == true)
+                {
+                    if (robotData.currAllBetChips == gameRoomData.currCallValue)
+                    {
+                        action = BetActingEnum.Check;
+                    }
+                    else
+                    {
+                        betValue = gameRoomData.currCallValue;
+                    }
+                }
+                else
+                {
+                    if (robotData.currAllBetChips == gameRoomData.currCallValue)
+                    {
+                        action = BetActingEnum.Check;
+                    }
+                    else
+                    {
+                        betValue = gameRoomData.currCallValue;
+                    }
+                }
+            }
+        }
+        else
+            action = BetActingEnum.Fold;
         #endregion
 
-        action = judgeAction(actionData.action);
-        if (actionData.raisePercentage != 0)
-        {
-            action = BetActingEnum.Raise;
-            double bb = gameRoomData.smallBlind * 2;
-            betValue = Mathf.Floor((float)(gameView.gameData.thisData.CurrRaiseValue + (bb * (double)actionData.raisePercentage / 100))); 
-        }
-        if (action == BetActingEnum.Call)
-            betValue = gameRoomData.currCallValue;
-        if (action == BetActingEnum.AllIn)
-            betValue = robotData.carryChips;
+        //action = judgeAction(actionData.action);
+        //if (actionData.raisePercentage != 0)
+        //{
+        //    action = BetActingEnum.Raise;
+        //    double bb = gameRoomData.smallBlind * 2;
+        //    betValue = Mathf.Floor((float)(gameView.gameData.thisData.CurrRaiseValue + (bb * (1 + (double)actionData.raisePercentage / 100))));
+        //}
+        //else if (action == BetActingEnum.Call)
+        //    betValue = gameRoomData.currCallValue;
+        //else if (action == BetActingEnum.AllIn)
+        //    betValue = robotData.carryChips;
 
-        print("實際下注: " + action);
+        //print("實際下注: " + action);
         //更新資料(機器人下注行為)
         gameControl.UpdateBetAction(gameRoomData.currActionerId,
                                     action,
