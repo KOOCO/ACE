05c708c6461592176b450bb173730cf2b098d6c1
unknown <king24371@gmail.com>
2025-05-28 16:38:50 +0800

    Ace-708

--- Assets/Scripts/Game/GameControl.cs
+++ Assets/Scripts/Game/GameControl.cs
@@ -1983,35 +1983,35 @@ public class GameControl : MonoBehaviour
             }
             else
             {
-                //機器人動作
-                if (player.UserId.StartsWith(FirebaseManager.ROBOT_ID) &&
-                    gameRoomData.actionCD == DataManager.RobotActionTime)
-                {
-                    string RT = RobotControl.handConvert(gameRoomData.playerDataDic[gameRoomData.currActionerId].handPoker);
-                    if (gameRoomData.currGameFlow < 3)
+                 //機器人動作
+                    if (player.UserId.StartsWith(FirebaseManager.ROBOT_ID) &&
+                        gameRoomData.actionCD == DataManager.RobotActionTime)
                     {
-                        print("機器人手牌" + RT);
-                        AppApi.robotPreFlop(new robotHand(RT), (x) =>
+                        string RT = RobotControl.handConvert(gameRoomData.playerDataDic[gameRoomData.currActionerId].handPoker);
+                        if (gameRoomData.currGameFlow < 3)
                         {
-                            print("機器人動作 " + x);
-                            RobotControl.RobotBet(gameRoomData, x);
-                        },
-                            (error) => Debug.LogError("機器人錯誤 " + error));
-                    }
-                    else
-                    {
-                        gameView.judgeRobotShape(player);
-                        print("機器人牌型" + player.pokerCurrShapeIndex);
-                        AppApi.robotAfterFlop(new robotShape(player.pokerCurrShapeIndex), (x) =>
+                            print("機器人手牌" + RT);
+                            AppApi.robotPreFlop(new robotHand(RT), (x) =>
+                            {
+                                print("機器人動作 " + x);
+                                RobotControl.RobotBet(gameRoomData, x);
+                            },
+                                (error) => Debug.LogError("機器人錯誤 " + error));
+                        }
+                        else
                         {
-                            print("機器人動作 " + x);
-                            RobotControl.RobotBet(gameRoomData, x);
-                        },
-                            (error) => Debug.LogError("機器人錯誤 " + error));
-                    }
+                            gameView.judgeRobotShape(player);
+                            print("機器人牌型" + player.pokerCurrShapeIndex);
+                            AppApi.robotAfterFlop(new robotShape(player.pokerCurrShapeIndex), (x) =>
+                            {
+                                print("機器人動作 " + x);
+                                RobotControl.RobotBet(gameRoomData, x);
+                            },
+                                (error) => Debug.LogError("機器人錯誤 " + error));
+                        }
 
-                    yield break;
-                }
+                        yield break;
+                    }
 
                 //更新倒數
                 var data = new Dictionary<string, object>()
